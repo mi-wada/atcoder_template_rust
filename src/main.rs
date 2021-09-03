985f24ff4bae4a153b6df0cbedbbb378abf305d7
mi-wada <mtak.wd@gmail.com>
2021-09-03 15:14:07 +0900

    solve abc214c

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn cin() -> String {
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).unwrap();
@@ -5,8 +7,9 @@ fn cin() -> String {
 }
 
 fn cin_vec<T>() -> Vec<T>
-    where T: std::str::FromStr,
-          T::Err: std::fmt::Debug
+where
+    T: std::str::FromStr,
+    T::Err: std::fmt::Debug,
 {
     cin()
         .split_whitespace()
@@ -15,20 +18,39 @@ fn cin_vec<T>() -> Vec<T>
 }
 
 fn main() {
-    let n: usize = cin().parse().unwrap();
-    let s = cin_vec::<i32>();
-    let t = cin_vec::<i32>();
-
-    let mut ans = vec![0; n as usize];
+    let hwn = cin_vec::<usize>();
+    let (_h, _w, n) = (hwn[0], hwn[1], hwn[2]);
+    let mut row = vec![0; n];
+    let mut column = vec![0; n];
     for i in 0..n {
-        ans[i] = t[i];
+        let ab = cin_vec::<i32>();
+        row[i] = ab[0];
+        column[i] = ab[1];
     }
 
-    for i in 0..2*n {
-        ans[(i + 1) % n] = std::cmp::min(ans[(i + 1) % n], ans[i % n] + s[i % n]);
-    }
+    let mut sorted_row = row.clone();
+    sorted_row.sort();
+    let row_compress: HashMap<i32, i32> = sorted_row
+        .iter()
+        .enumerate()
+        .map(|(i, v)| (*v, (i+1) as i32))
+        .collect();
+
+    let mut sorted_column = column.clone();
+    sorted_column.sort();
+    let column_compress: HashMap<i32, i32> = sorted_column
+        .iter()
+        .enumerate()
+        .map(|(i, v)| (*v, (i+1) as i32))
+        .collect();
+
+    println!("{:#?}", row_compress);
 
     for i in 0..n {
-        println!("{}", ans[i]);
+        println!(
+            "{} {}",
+            row_compress.get(&row[i]).unwrap(),
+            column_compress.get(&column[i]).unwrap()
+        );
     }
 }
