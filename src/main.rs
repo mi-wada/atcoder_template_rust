464ca4a4fc641810f34a1bf9f2dcd3d2ef32d1bb
mi-wada <mtak.wd@gmail.com>
2021-09-08 20:41:24 +0900

    solve arc213d

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 fn cin() -> String {
     let mut buf = String::new();
@@ -36,25 +36,31 @@ where
     (v[0], v[1])
 }
 
+fn dfs(g: &Vec<Vec<usize>>, from_idx: usize, current_idx: usize) {
+    print!("{} ", current_idx + 1);
+
+    for next_idx in g[current_idx].iter() {
+        if *next_idx != from_idx {
+            dfs(g, current_idx, *next_idx);
+        }
+    }
+    if !(from_idx == current_idx && from_idx == 0) {
+        print!("{} ", from_idx + 1);
+    }
+}
+
 fn main() {
     let n: usize = cin().parse().unwrap();
-    let (x, y): (Vec<i32>, Vec<i32>) = (0..n).map(|_| tuple_2(cin_vec::<i32>())).unzip();
-    let mut x_with_idx: Vec<(usize, i32)> = x.into_iter().enumerate().collect();
-    let mut y_with_idx: Vec<(usize, i32)> = y.into_iter().enumerate().collect();
-    x_with_idx.sort_by(|(_a_idx, a), (_b_idx, b)| a.cmp(b));
-    y_with_idx.sort_by(|(_a_idx, a), (_b_idx, b)| a.cmp(b));
-    let mut answer_candidate = vec![
-        x_with_idx[n - 1].1 - x_with_idx[0].1,
-        x_with_idx[n - 1].1 - x_with_idx[1].1,
-        x_with_idx[n - 2].1 - x_with_idx[0].1,
-        y_with_idx[n - 1].1 - y_with_idx[0].1,
-        y_with_idx[n - 1].1 - y_with_idx[1].1,
-        y_with_idx[n - 2].1 - y_with_idx[0].1,
-    ];
-    answer_candidate.sort();
-    if (x_with_idx[0].0, x_with_idx[n - 1].0) == (y_with_idx[0].0, y_with_idx[n - 1].0) {
-        println!("{}", answer_candidate[6 - 3]);
-    } else {
-        println!("{}", answer_candidate[6 - 2]);
+    let mut graph = vec![vec![]; n];
+    for _ in 0..n - 1 {
+        let a_b: Vec<usize> = cin_vec::<usize>().iter().map(|x| x - 1).collect();
+        graph[a_b[0]].push(a_b[1]);
+        graph[a_b[1]].push(a_b[0]);
     }
+    for i in 0..n {
+        graph[i].sort();
+    }
+
+    dfs(&graph, 0, 0);
+    println!("");
 }
