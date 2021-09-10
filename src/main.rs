4a78883364f96fd7e66b72ba715a2f45953fd888
mi-wada <mtak.wd@gmail.com>
2021-09-10 17:01:54 +0900

    impl ToTuple

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-use std::{
-    collections::{HashMap, HashSet, VecDeque},
-    iter::FromIterator,
-};
-
+#[allow(dead_code)]
 fn cin() -> String {
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).unwrap();
     buf.trim_end().to_string()
 }
 
+#[allow(dead_code)]
 fn cin_vec<T>() -> Vec<T>
 where
     T: std::str::FromStr,
@@ -20,6 +17,7 @@ where
         .collect()
 }
 
+#[allow(dead_code)]
 fn cin_vertical<T>(n: usize) -> Vec<T>
 where
     T: std::str::FromStr,
@@ -32,50 +30,28 @@ where
     res
 }
 
-// TODO: ToTupleトレイトにしてvecに実装しよう
-fn tuple_2<T>(v: Vec<T>) -> (T, T)
+trait ToTuple<T> {
+    fn to_2(self) -> (T, T);
+    fn to_3(self) -> (T, T, T);
+}
+
+impl<T> ToTuple<T> for Vec<T>
 where
-    T: Copy,
+    T: Clone,
 {
-    (v[0], v[1])
+    fn to_2(self) -> (T, T) {
+        (self.get(0).unwrap().clone(), self.get(1).unwrap().clone())
+    }
+
+    fn to_3(self) -> (T, T, T) {
+        (
+            self.get(0).unwrap().clone(),
+            self.get(1).unwrap().clone(),
+            self.get(2).unwrap().clone(),
+        )
+    }
 }
 
-fn prime_factors(mut x: usize) -> Vec<usize> {
-    let mut res = Vec::new();
-    for i in 2..=((x as f64).sqrt() as usize + 1) {
-        if x % i == 0 {
-            res.push(i);
-            while x % i == 0 {
-                x /= i;
-            }
-        }
-    }
-    if x != 1 {
-        res.push(x);
-    }
-    res
-}
-
-fn main() {
-    let (n, m) = tuple_2(cin_vec::<usize>());
-    let a = cin_vec::<usize>();
-
-    let mut ans: HashSet<usize> = (1..=m).collect();
-
-    for _a in a {
-        for prime_factor in prime_factors(_a) {
-            if ans.contains(&prime_factor) {
-                let mut mult = 1;
-                while prime_factor * mult <= m {
-                    ans.remove(&(prime_factor * mult));
-                    mult += 1;
-                }
-            }
-        }
-    }
-
-    println!("{}", ans.len());
-    let mut ans: Vec<usize> = ans.into_iter().collect();
-    ans.sort();
-    ans.iter().for_each(|x| println!("{}", x));
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    Ok(())
 }
